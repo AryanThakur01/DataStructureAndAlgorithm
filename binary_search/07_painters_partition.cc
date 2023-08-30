059dac1e9dc542721e65cf5e6c7cd4c426bb41b6
Aryan Thakur <aryan197297@gmail.com>
2023-08-31 00:50:34 +0530

    practice: solved painter partition problem

---
+++ binary_search/07_painters_partition.cc
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+bool isPaintingPossible(vector<int> boards, int workers, int size, int testCase){
+  int i = 0;
+  int sum = 0;
+  while(i<size){
+    if(boards[i] > testCase) return false;
+    sum += boards[i];
+    if(sum >testCase){
+      if(--workers == 0)
+        return false;
+      sum = boards[i];
+    }
+    i++;
+  }
+  return true;
+}
+
+int findLargestMinDistance(vector<int> &boards, int workers){
+  int n = boards.size();
+  int s = 0;
+  int e = 0;
+  for (int i = 0; i<n ; i++) e += boards[i];
+  int mid = s + (e-s)/2;
+  int ans = -1;
+  while(s<e){
+    if(isPaintingPossible(boards, workers, n, mid)){
+      ans = mid;
+      e = mid;
+    }
+    else
+      s = mid+1;
+    mid = s + (e-s)/2;
+  }
+  return ans;
+}
+
+int main () {
+  vector<int> boards = {10, 20, 30, 40};
+  int workers = 2;
+
+  int MinimumLargestDist = findLargestMinDistance(boards, workers);
+  cout << "The largest distance minimized to is: " << MinimumLargestDist;
+  return 0;
+}
