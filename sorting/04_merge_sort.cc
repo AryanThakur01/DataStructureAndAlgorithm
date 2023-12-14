f7612fc000f4f7ae2b3cfc907d4cc4b11c5d753a
Aryan Thakur <aryan197297@gmail.com>
2023-12-14 22:07:37 +0530

    update: the merge sort code for better understanding with comments

--- sorting/04_merge_sort.cc
+++ sorting/04_merge_sort.cc
@@ -1,52 +1,76 @@
 #include <iostream>
+
 using namespace std;
 
-void merge(int[], int, int, int);
-void mergeSort(int[], int, int);
+// ========================= HELPER FUNCTIONS =========================
+void mergeSort(int arr[], int start, int end);
+void merge(int arr[], int start, int mid, int end);
+// ====================================================================
+
+// ========================= DRIVER FUNCTION ==========================
 int main() {
-  int size = 10;
-  int arr[] = {-1, 1, 5, 3, 2, 0, 11, 12, 13, -10};
-  for (int i = 0; i < 10; i++)
+  int n;
+  cout << "Enter The Number of Elements: ";
+  cin >> n;
+
+  int arr[n];
+  cout << "Enter " << n << " space seperated characters: ";
+  for (int i = 0; i < n; i++)
+    cin >> arr[i];
+
+  for (int i = 0; i < n; i++)
     cout << arr[i] << " ";
   cout << endl;
 
-  mergeSort(arr, 0, size - 1);
+  mergeSort(arr, 0, n - 1);
 
-  for (int i = 0; i < size; i++)
+  for (int i = 0; i < n; i++)
     cout << arr[i] << " ";
+  cout << endl;
   return 0;
 }
+// ====================================================================
 
-void mergeSort(int arr[], int s, int e) {
-  if (s >= e)
+// --------------------------- Merge Sort -----------------------------
+void mergeSort(int arr[], int start, int end) {
+  // Base Condition
+  if (start >= end)
     return;
-  int mid = s + (e - s) / 2;
 
-  mergeSort(arr, s, mid);
-  mergeSort(arr, mid + 1, e);
-  merge(arr, s, e, mid);
-};
+  // Reccursive Call
+  int mid = start + (end - start) / 2;
+  mergeSort(arr, start, mid);
+  mergeSort(arr, mid + 1, end);
 
-void merge(int arr[], int s, int e, int mid) {
-  int s1 = mid - s + 1, s2 = e - mid;
-  int arr1[s1], arr2[s2];
-
-  // make the sub arrays
-  for (int i = 0; i < s1; i++)
-    arr1[i] = arr[s + i];
-  for (int i = 0; i < s2; i++)
-    arr2[i] = arr[mid + i + 1];
-
-  // Merge the 2 arrays
-  int i = 0, j = 0, k = s;
-  while (i < s1 && j < s2) {
-    if (arr1[i] < arr2[j])
-      arr[k++] = arr1[i++];
-    else
-      arr[k++] = arr2[j++];
-  }
-  while (i < s1)
-    arr[k++] = arr1[i++];
-  while (j < s2)
-    arr[k++] = arr2[j++];
+  // Action Function
+  merge(arr, start, mid, end);
 }
+// --------------------------------------------------------------------
+
+// ------------------------ Merge Sub Arrays --------------------------
+void merge(int arr[], int start, int mid, int end) {
+  int sizeLeft = mid - start + 1, sizeRight = end - mid;
+  int left[sizeLeft], right[sizeRight];
+
+  // Make the sub arrays
+  for (int i = 0; i < sizeLeft; i++)
+    left[i] = arr[i + start];
+  for (int i = 0; i < sizeRight; i++)
+    right[i] = arr[i + mid + 1];
+
+  // Sorted Merging
+  int leftIt = 0, rightIt = 0, mainIt = start;
+  while (leftIt < sizeLeft && rightIt < sizeRight) {
+    if (left[leftIt] < right[rightIt])
+      arr[mainIt++] = left[leftIt++];
+    else
+      arr[mainIt++] = right[rightIt++];
+  }
+
+  // If any elements left unmerged the merge them
+  while (leftIt < sizeLeft)
+    arr[mainIt++] = left[leftIt++];
+  while (rightIt < sizeRight)
+    arr[mainIt++] = right[rightIt++];
+};
+// --------------------------------------------------------------------
